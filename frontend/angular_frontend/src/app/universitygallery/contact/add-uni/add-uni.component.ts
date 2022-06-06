aa50a262eb4527ffe5da25170a677aeaa1d829ef
sorennx <sorennx@gmail.com>
2022-06-06 22:30:16 +0200

    Country - Region - City cleaned up console.logs()

--- frontend/angular_frontend/src/app/universitygallery/contact/add-uni/add-uni.component.ts
+++ frontend/angular_frontend/src/app/universitygallery/contact/add-uni/add-uni.component.ts
@@ -7,13 +7,13 @@ import { SharedService } from 'src/app/shared.service';
   styleUrls: ['./add-uni.component.css']
 })
 export class AddUniComponent implements OnInit {
-  
+
 
   constructor(private service: SharedService) { }
 
-  @Input() dep:any;
+  @Input() dep: any;
   UniversityName: string = '';
-  CityId: string = '';
+  CityId: number = 0;
   UniversityGeneralDescription: string = '';
   UniversityLogoLink: string = '';
   UniversityMainPageLink: string = '';
@@ -21,23 +21,23 @@ export class AddUniComponent implements OnInit {
   UniversityContactInfo: string = '';
 
   Country: string = '';
-  CountryList: any =[];
-  CountryListNotFiltered: any =[];
+  CountryList: any = [];
+  CountryListNotFiltered: any = [];
   Region: string = '';
-  RegionList: any =[];
-  RegionListNotFiltered: any =[];
-  RegionListFiltered: any =[];
+  RegionList: any = [];
+  RegionListNotFiltered: any = [];
+  RegionListFiltered: any = [];
   City: string = '';
-  CityList: any =[];
-  CityListNotFiltered: any =[];
-  CityListFiltered: any=[];
+  CityList: any = [];
+  CityListNotFiltered: any = [];
+  CityListFiltered: any = [];
 
   ngOnInit(): void {
     this.getCountryList();
     // this.getRegionList();
     // this.getCityList();
   }
-  
+
 
   addUniversity() {
     var val = {
@@ -49,71 +49,58 @@ export class AddUniComponent implements OnInit {
       UniversityCoursesLink: this.UniversityCoursesLink,
       UniversityContactInfo: this.UniversityContactInfo
     };
-    
-    this.service.addUniversity(val).subscribe((res: { toString: () => any; })=>{
+
+    this.service.addUniversity(val).subscribe((res: { toString: () => any; }) => {
       alert(res.toString());
     })
   }
-  getCountryList(){
+  getCountryList() {
     this.service.getCountryList().subscribe(data => {
       this.CountryList = data;
       this.CountryListNotFiltered = data;
-      
+
     });
     return 0;
   }
 
-  getRegionList(){
+  getRegionList() {
     this.service.getRegionList().subscribe(data => {
       this.RegionList = data;
       this.RegionListNotFiltered = data;
-      console.log("2",this.RegionList);
     });
   }
 
-  getCityList(){
+  getCityList() {
     this.service.getCityList().subscribe(data => {
       this.CityList = data;
       this.CityListNotFiltered = data;
     });
   }
   delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
-  async onCountrySelect(){
-    console.log("1");
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  async onCountrySelect() {
     this.getRegionList();
     await this.delay(50);
-    
-    console.log("3", this.RegionList);
 
-    var regionsFiltered = this.RegionListNotFiltered.filter( (el: { CountryId: number; }) =>{
+    var regionsFiltered = this.RegionListNotFiltered.filter((el: { CountryId: number; }) => {
       return el.CountryId == parseInt(this.Country);
     });
     this.RegionListFiltered = regionsFiltered;
-    console.log('after filtering: ',this.RegionList);
   }
 
-  async onRegionSelect(){
-    console.log("Region selection made");
+  async onRegionSelect() {
     this.getCityList();
     await this.delay(50);
 
-    var citiesFiltered = this.CityListNotFiltered.filter( (el: { RegionId: number; }) =>{
+    var citiesFiltered = this.CityListNotFiltered.filter((el: { RegionId: number; }) => {
       return el.RegionId == parseInt(this.Region);
     });
     this.CityListFiltered = citiesFiltered;
-    console.log('after filtering: ',this.CityList);
   }
 
-  
-  
-  test(){
-    console.log(">>>Test",parseInt(this.Country),parseInt(this.Region),parseInt(this.City));
-    
-
-    
-    
-
+  async onCitySelect() {
+    this.CityId = parseInt(this.City);
+    await this.delay(50);
   }
 }
